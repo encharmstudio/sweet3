f8fbdc6f32229b4e9e5b3d42778e4a0c31b74a59
dre_mechanics <andrey.sychenko@gmail.com>
2022-09-14 00:42:43 +0300

    GPGPU util

---
+++ src/gl/util/gpgpu.js
@@ -0,0 +1,78 @@
+import { ClampToEdgeWrapping, FloatType, LinearEncoding, Mesh, NearestFilter, OrthographicCamera, PlaneBufferGeometry, RawShaderMaterial, RedFormat, WebGLRenderTarget } from "three";
+import { Root } from "../Root";
+
+// this.plane = Geometry({
+//   position: {
+//     size: 2,
+//     data: new Float32Array([-1, -1, 3, -1, -1, 3]),
+//   },
+//   uv: {
+//     size: 2,
+//     data: new Float32Array([0, 0, 2, 0, 0, 2]),
+//   },
+// })
+const plane = new PlaneBufferGeometry(2, 2, 1, 1);
+const dummyCam = new OrthographicCamera(-1, 1, 1, -1, -1, 1);
+
+export const createRT = ({
+  width,
+  height,
+  filter = NearestFilter,
+  format = RedFormat,
+  type = FloatType,
+  wrapping = ClampToEdgeWrapping,
+} = {}) => {
+  const t = new WebGLRenderTarget(width, height, {
+    type,
+    format,
+    minFilter: filter,
+    magFilter: filter,
+    encoding: LinearEncoding,
+    depthBuffer: false,
+  });
+  t.texture.wrapS = t.texture.wrapT = wrapping;
+  return t;
+};
+
+export const createDFBO = ({
+  width,
+  height,
+  filter = NearestFilter,
+  format = RedFormat,
+  type = FloatType,
+  wrapping = ClampToEdgeWrapping,
+} = {}) => {
+  return {
+    read: createRT({ width, height, filter, format, type, wrapping }),
+    write: createRT({ width, height, filter, format, type, wrapping }),
+    swap() {
+      const t = this.read;
+      this.read = this.write;
+      this.write = t;
+    },
+    isDFBO: true,
+  };
+};
+
+export const createProgram = ({ vertex, fragment, uniforms, geometry = plane }) => new Mesh(
+  geometry,
+  new RawShaderMaterial({
+    vertexShader: vertex,
+    fragmentShader: fragment,
+    uniforms,
+    depthTest: false,
+    depthWrite: false,
+  })
+);
+
+export const renderFBO = (program, target) => {
+  
+  const renderTarget = target.isDFBO ? target.write : target;
+
+  Root.renderPipe.renderer.setRenderTarget(renderTarget);
+  Root.renderPipe.renderer.render(program, dummyCam);
+
+  if (target.isDFBO) {
+    target.swap();
+  }
+};
